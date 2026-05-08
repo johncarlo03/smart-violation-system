c1c1c2b790f2509c243930cf80cd116b96d3ad1a
johncarlo03 <120325948+johncarlo03@users.noreply.github.com>
2026-05-09 01:21:41 +0800

    for fixing redirect too many times bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-        'restrict' => \App\Http\Middleware\EnsureSAO::class,
+        'role' => \App\Http\Middleware\RoleMiddleware::class,
     ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {

b4f11f0fc882c3b5828ba3e026ffdf35c44f2e73
johncarlo03 <120325948+johncarlo03@users.noreply.github.com>
2026-06-16 00:14:57 +0800

    optimized code

--- database/seeders/OffensePenaltySeeder.php
+++ database/seeders/OffensePenaltySeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Offense;
+use \App\Models\OffensePenalty;
 use Illuminate\Database\Seeder;
+
 
 class OffensePenaltySeeder extends Seeder
 {
@@ -12,25 +15,46 @@ class OffensePenaltySeeder extends Seeder
     public function run(): void
     {
         // Find 'Smoking on Campus'
-        $assault = \App\Models\Offense::where('name', 'Physical Assault')->first();
+        $assault = Offense::where('name', 'Physical Assault')->first();
+        $studentId = Offense::where('name', 'ID Policy Non-compliance')->first();
 
         if ($assault) {
-            \App\Models\OffensePenalty::create([
+            OffensePenalty::create([
                 'offense_id' => $assault->id,
                 'level' => 1,
                 'penalty_description' => 'Suspension for 5 days.'
             ]);
 
-            \App\Models\OffensePenalty::create([
+            OffensePenalty::create([
                 'offense_id' => $assault->id,
                 'level' => 2,
                 'penalty_description' => 'Suspension for 1 semester.'
             ]);
 
-            \App\Models\OffensePenalty::create([
+            OffensePenalty::create([
                 'offense_id' => $assault->id,
                 'level' => 3,
                 'penalty_description' => 'Expulsion'
+            ]);
+        }
+
+        if ($studentId) {
+            OffensePenalty::create([
+                'offense_id' => $studentId->id,
+                'level' => 1,
+                'penalty_description' => 'Campus entry authorized. Requires signed undertaking at Dean’s Office'
+            ]);
+
+            OffensePenalty::create([
+                'offense_id' => $studentId->id,
+                'level' => 2,
+                'penalty_description' => 'Campus entry authorized. Requires signed undertaking at Dean’s Office'
+            ]);
+
+            OffensePenalty::create([
+                'offense_id' => $studentId->id,
+                'level' => 3,
+                'penalty_description' => '5 hours of University Service to be completed within 1 week. Consecutive offenses incur an additional 5 hours per violation.'
             ]);
         }
     }

7fd81df333877387ee44936e1979b83d2334e8ee
johncarlo03 <120325948+johncarlo03@users.noreply.github.com>
2026-07-15 01:47:15 +0800

    Update Offense.php

--- app/Models/Offense.php
+++ app/Models/Offense.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offense extends Model
 {
+    protected $fillable = ['name', 'category'];
     public function penalties()
 {
     // One offense has many possible penalty levels

bd08cd5be34ece852254cd6b589edee31b822dcc
johncarlo03 <120325948+johncarlo03@users.noreply.github.com>
2026-07-15 01:48:23 +0800

    Update superadmin-nav.blade.php
    fix for route optimization

--- resources/views/layouts/superadmin-nav.blade.php
+++ resources/views/layouts/superadmin-nav.blade.php
@@ -43,23 +43,28 @@
                 
 
                 <x-sidebar-link
-                route="superadmin.users"
-                active="superadmin.users*">
+                route="superadmin.users.index"
+                active="superadmin.users.index*">
                     <svg class="w-5 h-5 text-gray-400 group-hover:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
                     </svg>
                     {{ __('User Directory Governance') }}
                 </x-sidebar-link>
+                
 
-                <a href="#"
-                    class="flex items-center gap-3 px-4 py-3 text-gray-500 hover:text-gray-800 hover:bg-gray-50 rounded-xl font-bold text-sm transition duration-200 group">
+                <x-sidebar-link
+                route="superadmin.offenses.index"
+                active="superadmin.offenses.index*">
                     <svg class="w-5 h-5 text-gray-400 group-hover:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+                            d="M12 15v2m0-6v2m0-6a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+                            d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                     </svg>
-                    {{ __('System Triggers') }}
-                </a>
+                    {{ __('Offenses') }}
+                </x-sidebar-link>
+                
             </nav>
 
             <div class="p-4 border-t border-gray-100 bg-gray-50/50 flex flex-col gap-3">
